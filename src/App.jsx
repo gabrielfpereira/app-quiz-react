317d3f7c8bd0ca19bd1e1ecba366546505fd747c
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-09-25 12:30:36 -0300

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { useContext, useEffect } from 'react'
+import { QuizContext } from './context/quizContext'
 
 import Welcome from './components/welcome'
+import Question from './components/Question'
+import GameOver from './components/GameOver'
 
 function App() {
+  const [quizstate, dispatch] = useContext(QuizContext)
+
+  useEffect(()=>{
+    dispatch({type: 'REORDER_QUESTIONS'})
+  },[])
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Welcome/>
+      { quizstate.gameStage == 'Start' && <Welcome/> }
+      { quizstate.gameStage == 'Playing' && <Question/> }
+      { quizstate.gameStage == 'End' && <GameOver/> }
     </div>
   )
 }
