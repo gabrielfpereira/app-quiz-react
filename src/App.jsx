c1ae900421d6492c5174519ff6562fd4527f7e55
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-09-25 09:56:05 -0300

    UI initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 
+import Welcome from './components/welcome'
+
 function App() {
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
+      <Welcome/>
     </div>
   )
 }
---
+++ src/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './welcome.css'
+
+const welcome = () => {
+  return (
+    <div id='welcome'>
+        <h2>Seja Bem-Vindo!</h2>
+        <p>clique no botão abaixo para começar</p>
+        <button>iniciar</button>
+    </div>
+  )
+}
+
+export default welcome
