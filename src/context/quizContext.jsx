86ffe5ff3210964d92b7fec473bed99b3e4d4682
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-09-25 12:29:16 -0300

    reducer

--- src/context/quizContext.jsx
+++ src/context/quizContext.jsx
@@ -1,19 +1,57 @@
 import { createContext, useReducer } from "react";
 import questions from '../data/questions'
 
-const STAGES = ['Start', 'playing', 'End']
+const STAGES = ['Start', 'Playing', 'End']
 
 const initialState = {
     gameStage : STAGES[0],
-    questions
+    questions,
+    currentQuestion: 0,
+    score: 0,
+    answerSelected: false
 }
 
 const quizReducer = (state, action) => {
-    console.log(state, action);
 
     switch (action.type) {
         case 'CHANGE_STATE':
-            return state    
+            return {...state, gameStage: STAGES[1]}
+
+        case 'REORDER_QUESTIONS':
+            const reordered_questions = questions.sort(()=>{
+               return Math.random() - 0.5
+            })
+
+            return {...state, questions: reordered_questions}
+
+        case 'CHANGE_QUESTION':
+            const nextCurrentQuesiton = state.currentQuestion + 1
+            let endGame = false
+
+            if(!questions[nextCurrentQuesiton]){
+                endGame = true
+            }
+            return {
+                ...state, 
+                currentQuestion: nextCurrentQuesiton, 
+                gameStage: endGame ? STAGES[2] : state.gameStage,
+                answerSelected: false
+            }
+
+        case 'RESTART':
+            return initialState
+        
+        case 'CHECK_ANSWER':
+            if(state.answerSelected) return state
+            let correctAnswer = 0
+
+            if(action.payload.answer == action.payload.option) correctAnswer = 1
+
+            return {
+                ...state,
+                score: state.score + correctAnswer,
+                answerSelected: action.payload.option
+            }
         default:
             return state
     }
